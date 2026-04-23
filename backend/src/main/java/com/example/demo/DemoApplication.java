eba6d68bbd45be88d58b379c9a445ec38b61b6fe
amelkouidri <amelkouidri280@gmail.com>
2026-04-23 16:58:33 +0200

    Add Ansible deployment in CI/CD pipeline

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootApplication
-public class DemoApplication {
+@Testcontainers
+@SpringBootTest
+class DemoApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.2-alpine")
+            .withDatabaseName("db")
+            .withUsername("usr")
+            .withPassword("pwd");
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
